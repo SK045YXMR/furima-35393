a7ebadb1f2853f1818829381ecf242f781bde5b6
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-04-27 21:46:57 +0900

    Userモデルへのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+          with_options presence: true do
+            validates :nickname
+            validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'First name Full-width characters' }
+            validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name Full-width characters' }
+            validates :first_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'First name kana Full-width katakana characters' }
+            validates :last_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'Last name kana Full-width katakana characters' }
+            validates :birth_date
+          end
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
+
 end
