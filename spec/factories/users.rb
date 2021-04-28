6e9eb57e1447eea2a14ee21bcd53ce64494dab20
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-04-28 15:57:54 +0900

    Userモデルのテストコードの準備及びGimeiの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name}
+    email {Faker::Internet.free_email}
+    password {'test1234TEST'}
+    password_confirmation {password}
+    last_name {Gimei.last.kanji}
+    first_name {Gimei.first.kanji}
+    last_name_kana {Gimei.last.katakana}
+    first_name_kana {Gimei.first.katakana}
+    birth_date {"1991-01-01"}
+  end
+end
