715b91a4a8a1a4b07eaf83671767fe52336b831c
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-04-28 17:11:19 +0900

    Userモデルの単体テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録ができるとき' do
+      it 'ユーザー情報と本人情報が全て入力されていれば登録できる' do
+      end
+    end
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+      end
+      it 'emailが空では登録できない' do
+      end
+      it 'emailに@を含まないと登録できない' do
+      end
+      it 'emailが重複すると登録できない' do
+      end
+      it 'passwordが空では登録できない' do
+      end
+      it 'passwordが5文字以下では登録できない' do
+      end
+      it 'passwordは半角英数字が混合されてないと登録できない' do
+      end
+      it 'passwordとpassword_confirmationが一致しないと登録できない' do
+      end
+      it 'first_nameとlast_nameが空では登録できない' do
+      end
+      it 'first_nameとlast_nameは漢字・ひらがな・カタカナでなければ登録できない' do
+      end
+      it 'first_name_kanaとlast_name_kanaが空では登録できない' do
+      end
+      it 'first_name_kanaとlast_name_kanaがカタカナでなければ登録できない' do
+      end
+      it 'birth_dateが空では登録できない' do
+      end
+    end
+  end
 end
