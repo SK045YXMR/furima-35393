76c0b168c5b098bf72d71513f525818e27b86a18
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-04-28 19:30:07 +0900

    Userテストコード及びバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
           with_options presence: true do
             validates :nickname
-            validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'First name Full-width characters' }
-            validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name Full-width characters' }
-            validates :first_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'First name kana Full-width katakana characters' }
-            validates :last_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'Last name kana Full-width katakana characters' }
+            validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' }
+            validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' }
+            validates :first_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'Full-width katakana characters' }
+            validates :last_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'Full-width katakana characters' }
             validates :birth_date
           end
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
