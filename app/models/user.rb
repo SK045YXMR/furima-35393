0f896c228528fe572662fa63219dc7f97bda33ff
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-04-28 11:37:36 +0900

    アソシエーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
             validates :birth_date
           end
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
+  has_many :items
+  has_many :purchases
 
 end
