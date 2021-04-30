52bb64b86de814a1e61d6b460898aa38384a4742
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-04-30 18:08:42 +0900

    バリデーションの修正と一部変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :shopping_charges
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  belongs_to :user
+  has_one :purchase
+
+  has_one_attached :image
+
   with_options presence: true do
     validates :item_name
     validates :item_example
@@ -14,18 +26,7 @@ class Item < ApplicationRecord
     validates :days_to_ship_id
   end
 
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
-  validates :price, length: { in: 3..7 }
-  
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :item_category_id
-  belongs_to :item_status_id
-  belongs_to :shopping_charges_id
-  belongs_to :shipping_area_id
-  belongs_to :days_to_ship_id
 
-  belongs_to :user
-  has_one :purchase
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, length: { in: 3..7 }, numericality: { only_integer: true, greater_than: 300, less_than: 10000000 }
 
-  has_one_attached :image
 end
