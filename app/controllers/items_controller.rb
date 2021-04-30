fc4d498f3732780751557caa5546008e4c2ba59b
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-04-30 18:08:18 +0900

    商品出品機能のアクション記述とページの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_example, :item_category_id, :item_status_id, :shopping_charges_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
 end
