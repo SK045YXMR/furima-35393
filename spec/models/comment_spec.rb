8cc7ded351ddadf3d136d266fc59ae80f3ffc032
SK045YXMR <81885686+SK045YXMR@users.noreply.github.com>
2021-05-11 15:56:00 +0900

    commentモデルの単体テストコード

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント機能' do
+    context 'コメントができるとき' do
+      it 'コメントが適切に入力されていればコメントできる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントができないとき' do
+      it 'コメントが空では投稿できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'ユーザーが紐づかなければコメントできない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("User must exist")
+      end
+      it '商品が紐づかなければコメントできない' do
+        @comment.item = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Item must exist")
+      end
+    end
+  end
 end
